0a5a686206183dc2fd416fb5264ccd8bfff52e37
Iqbal Setiawan <ibal.setiawan08@gmail.com>
2025-08-02 21:00:33 +0700

    feat: add properties collection labels

--- src/components/PageRange/index.tsx
+++ src/components/PageRange/index.tsx
@@ -10,6 +10,10 @@ const defaultCollectionLabels = {
     plural: 'Posts',
     singular: 'Post',
   },
+  properties: {
+    plural: 'Properties',
+    singular: 'Property',
+  },
 }
 
 export const PageRange: React.FC<{
